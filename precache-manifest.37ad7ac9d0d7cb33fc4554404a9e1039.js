f91a14d7496e897e933baf24e5e8fb69a7811eae
yesseri <henrik.zenkert@gmail.com>
2020-10-09 23:48:33 +0200

    Updates

---
+++ precache-manifest.37ad7ac9d0d7cb33fc4554404a9e1039.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e0a4f604bf2e4960603ffc0c89ea9e",
+    "url": "/singer-page/index.html"
+  },
+  {
+    "revision": "f74fe95089501428c4fa",
+    "url": "/singer-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "76e2b4eda7110f6115b1",
+    "url": "/singer-page/static/js/2.a6725cb6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/singer-page/static/js/2.a6725cb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f74fe95089501428c4fa",
+    "url": "/singer-page/static/js/main.e02a976c.chunk.js"
+  },
+  {
+    "revision": "18e0e3553142af86837c",
+    "url": "/singer-page/static/js/runtime-main.60433e94.js"
+  }
+]);
