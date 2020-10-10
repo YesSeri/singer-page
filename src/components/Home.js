75ac01ee7886089fa5af54a24b08d9066344cab4
yesseri <henrik.zenkert@gmail.com>
2020-10-10 13:30:35 +0200

    Trying to deploy heroku

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <>
             <h1>Henrik Zenkert</h1>
-            <h2>Opera and liedaa</h2>
+            <h2>Opera and lied</h2>
             <img src="./cropped_pic.jpg" alt="" />
         </>
     )
