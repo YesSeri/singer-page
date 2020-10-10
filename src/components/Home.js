a471b2d0a9e4c0255e46ab237d4aabcfa12412c8
yesseri <henrik.zenkert@gmail.com>
2020-10-10 14:37:37 +0200

    CSSed the youtube videos

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,16 +7,34 @@ const Image = styled.img`
   height: auto;
 `;
 const VideoContainer = styled.div`
+	display:flex;
+  @media (max-width: 768px) {
+		display: block; 
+  }
+`;
+const Video = styled.div`
+	flex: 1; 
+	margin: 1%;
 `;
 
 export default function Home() {
+	const handlePlay = () => {
+		console.log("VideoPlayed")
+	}
+	const handlePause = () => {
+		console.log("VideoPause")
+	}
 	return (
 		<>
-
 			<h2>Opera and lied</h2>
 			<Image src="./cropped_pic.jpg" alt="" />
 			<VideoContainer>
-				<Youtube />
+				<Video>
+					<Youtube videoId="IkMXMEplOIo" handlePlay={handlePlay} handlePause={handlePause}/>
+				</Video>
+				<Video>
+					<Youtube videoId="IkMXMEplOIo" handlePlay={handlePlay} handlePause={handlePause}/>
+				</Video>
 			</VideoContainer>
 		</>
 	)
--- src/components/Youtube.js
+++ src/components/Youtube.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import YouTube from 'react-youtube';
+import styled from 'styled-components'
+const VideoContainer = styled.div`
+	display: ${(props) => props.showVideo ? 'block' : 'none'};
+	position: relative;
+	width: 100%;
+	height: 0;
+	padding-bottom: 56.25%;
+	overflow: hidden;
+	margin-bottom: 50px;
 
+	iframe {
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
+`
 
+export default function Youtube({ handlePlay, handlePause, videoId }) {
+	const [showVideo, setShowVideo] = useState(false);
+	const opts = {
+		playerVars: {
+			autoplay: 0,
+		},
+	};
 
-export default function Youtube() {
-    const opts = {
-        playerVars: {
-            autoplay: 0,
-        },
-    };
+	return (
+		<VideoContainer showVideo={showVideo}>
+			<YouTube onPlay={handlePlay} onPause={handlePause} videoId={videoId} opts={opts} onReady={()=>setShowVideo(true)} />;
+		</VideoContainer>
 
-    return <YouTube videoId="IkMXMEplOIo" opts={opts} />;
+	)
 
 }
